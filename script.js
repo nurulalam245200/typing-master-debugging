07329d4afe53d6429aaaf9509d8aef169a473e84
nurulalam <jack245200@gmail.com>
2022-09-15 03:08:21 +0600

    blog.html complete

--- script.js
+++ script.js
@@ -50,9 +50,6 @@ const typeController = (e) => {
     display.innerHTML += `<span class="red">${
       newLetter === " " ? "▪" : newLetter
     }</span>`;
-    const incorrect = (newLetter) => {
-      newLetter++;
-    };
   }
 
   // check if given question text is equal to user typed text
