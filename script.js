913d1e311ea68c2c050a089f5f3fca37e424e86f
nurulalam <jack245200@gmail.com>
2022-09-15 14:54:41 +0600

    time show in history fixed

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${Math.round(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -140,6 +140,6 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
   document.getElementById("show-time").innerHTML = `${
-    startTime ? Math.round(timeSpent) : 0
+    startTime ? timeSpent : 0
   } seconds`;
 }, 1000);
